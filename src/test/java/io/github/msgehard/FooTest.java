06eb7c180034ec101273d10bfe68bc4d9bdaa2bc
Mike Gehard <mike.gehard@gmail.com>
2014-05-31 14:09:02 -0600

    Get JUnit4 tests set up

---
+++ src/test/java/io/github/msgehard/FooTest.java
@@ -0,0 +1,19 @@
+package com.example.foo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
+ */
+@RunWith(JUnit4.class)
+public class FooTest {
+
+    @Test
+    public void thisAlwaysPasses() {
+        assert true;
+    }
+}
